85d761537b102dff7bd91e626a161648626a283f
Marnie Reid <marnie@kodewithklossy.com>
2023-05-01 12:20:25 +0200

    Initial Commit

---
+++ toDoListForLesson/toDoListForLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListForLessonApp.swift
+//  toDoListForLesson
+//
+//  Created by scholar on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListForLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
